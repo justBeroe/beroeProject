6ac3f87f577d94eedeb0fd5ac9caffc45a8b8a1f
beroe <dobromirtt@gmail.com>
2024-07-15 10:00:28 +0300

    Weather REST API was used to get City and put to Profile. City should change dynamically when you change City.

--- src/main/java/bg/softuni/beroe/web/UserController.java
+++ src/main/java/bg/softuni/beroe/web/UserController.java
@@ -1,6 +1,8 @@
 package bg.softuni.beroe.web;
 
 import bg.softuni.beroe.service.UserService;
+import bg.softuni.beroe.service.WeatherService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,9 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
 
     private final UserService userService;
+    private final WeatherService weatherService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, WeatherService weatherService) {
         this.userService = userService;
+        this.weatherService = weatherService;
     }
 
     @GetMapping("/users/profile")
@@ -19,6 +23,7 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView("profile");
 
         modelAndView.addObject("profileData", userService.getProfileData());
+        modelAndView.addObject("profileDataCityOnly", weatherService.fetchCity().getCity());
 
         return modelAndView;
     }
